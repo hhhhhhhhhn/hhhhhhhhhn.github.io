af8c4cec911b9236ceb92b992a38f98ff58c32fd
hhhhhhhhhn <joseugarte2262005@gmail.com>
2021-01-30 19:28:17 -0300

    finished main page

--- build.sh
+++ build.sh
@@ -10,14 +10,24 @@ cp -r "$SRC/" "$DIST/"
 
 IFS=$'\n'
 
-for file in `find -L "$DIST/" -type f`; do
-	cat "$file" | tr "\n" "\f" > /tmp/bush
+function template() { # writes to /tmp/bush
+	cat | tr "\n" "\f" > /tmp/bush
 	cat /tmp/bush > "$file"
 	for script in `cat "$file" | grep -o "{{[^}]*}}"`; do
 		echo "${script:2:-2}" | tr "\f" "\n" > /tmp/bush.sh
 		chmod +x /tmp/bush.sh
-		. /tmp/bush.sh | tr "\n" "\f" | xargs -I {} sed -i "s/${script}/{}/" "$file"
+		. /tmp/bush.sh | tr "\n" "\f" | head -c -1 \
+			| xargs -I {} sed -i "s/${script}/{}/" "$file"
 	done
 	cat "$file" | tr "\f" "\n" > /tmp/bush
+}
+
+for file in `find -L "$DIST/" -type f -not -path '*/\.*'`; do
+	template <"$file"
 	cat /tmp/bush > "$file"
+done
+
+for file in `find -L "$DIST/" -type f -path '*/\.*'`; do
+	. "$file"
+	rm "$file"
 done
